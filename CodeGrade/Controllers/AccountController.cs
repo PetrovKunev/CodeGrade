12647b4702a95fa43d1e245bd2aad89b87594fb3
Yavor Petrov <147103195+PetrovKunev@users.noreply.github.com>
2025-07-04 15:16:36 +0300

    Добавен базов AccountController с Login и Register действия и подобрения по изгледите

--- CodeGrade/Controllers/AccountController.cs
+++ CodeGrade/Controllers/AccountController.cs
@@ -1,1 +1,44 @@
- 
+using Microsoft.AspNetCore.Mvc;
+using CodeGrade.ViewModels;
+
+namespace CodeGrade.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Добавете логика за автентикация
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Добавете логика за регистрация
+                return RedirectToAction("Login");
+            }
+            return View(model);
+        }
+    }
+} 
